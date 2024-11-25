7af715e0eb7d72d36236e746cc42222522eb0509
Marc Donner <marc.donner@gmail.com>
2024-11-25 18:45:45 -0500

    tape cell alignment is better now.

--- cnc_flask.py
+++ cnc_flask.py
@@ -60,14 +60,15 @@ def index():
         resp = make_response(render_template('cnc-35.html',
             stack=cnc_engine.stack,
             appname=APPLICATION_NAME,
-            tape=cnc_engine.log))
+            tape=cnc_engine.log.log_list))
         resp.set_cookie('cnc_stack', cnc_engine.stack.stack_to_json())
     else:
         cnc_engine.stack.load_stack_from_json(cnc_stack_json)
     resp = make_response(render_template('cnc-35.html',
             stack=cnc_engine.stack,
             appname=APPLICATION_NAME,
-            tape=cnc_engine.log))
+            tape=cnc_engine.log.log_list[-20:]))
+    print(f"tape: {cnc_engine.log.log_list}")
     return resp
 
 @app.route("/", methods=["POST"])
@@ -104,4 +105,4 @@ def status():
     # print(f"cookie_value: {cnc_stack_json}")
     return render_template('status.html',
                            environ=os.environ,
-                           cookie=cnc_stack_json)
+                           cnc_stack=cnc_stack_json)
